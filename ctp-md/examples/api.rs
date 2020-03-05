f946a8d35fe5ebca7ddcd0ce547cca2f5cac245d
caleb.xiang <caleb.xiang@gmail.com>
2020-03-05 13:58:46 +0800

    update try url

--- ctp-md/examples/api.rs
+++ ctp-md/examples/api.rs
@@ -6,37 +6,40 @@ struct Spi;
 impl MdSpi for Spi {}
 
 fn main() {
-    let flow_path = ::std::ffi::CString::new("").unwrap();
+    let flow_path = std::ffi::CString::new("").unwrap();
     let mut md_api = MdApi::new(flow_path, false, false);
     md_api.register_spi(Box::new(Spi));
-    md_api.register_front(std::ffi::CString::new("tcp://180.168.146.187:10110").unwrap());
+    md_api.register_front(std::ffi::CString::new("tcp://180.168.146.187:10131").unwrap());
     md_api.init();
-    
+
     sleep(1);
+    println!("try req_user_login");
     match md_api.req_user_login(&Default::default(), 1) {
         Ok(()) => println!("req_user_login ok"),
         Err(err) => println!("req_user_login err: {:?}", err),
     };
 
     sleep(1);
+    println!("try subscribe_market_data");
     let instrument_ids = vec!(CString::new("IF1703").unwrap(),
                               CString::new("au1712").unwrap(),
                               CString::new("m1709").unwrap(),
                               CString::new("CF709").unwrap());
-
     match md_api.subscribe_market_data(&instrument_ids.clone()) {
         Ok(()) => println!("subscribe_market_data ok"),
         Err(err) => println!("subscribe_market_data err: {:?}", err),
     };
 
     sleep(1);
+    println!("try subscribe_for_quote_rsp");
     match md_api.subscribe_for_quote_rsp(&instrument_ids) {
         Ok(()) => println!("subscribe_for_quote_rsp ok"),
         Err(err) => println!("subscribe_for_quote_rsp err: {:?}", err),
     };
 
     
-    sleep(5);
+    sleep(3);
+    println!("try req_user_logout");
     match md_api.req_user_logout(&Default::default(), 2) {
         Ok(()) => println!("req_user_logout ok"),
         Err(err) => println!("req_user_logout err: {:?}", err),

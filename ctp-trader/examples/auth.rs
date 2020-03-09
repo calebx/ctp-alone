7e980e30263f9f29a5aabffb315b69260301883b
caleb.xiang <caleb.xiang@gmail.com>
2020-03-09 20:15:33 +0800

    updata password

--- ctp-trader/examples/auth.rs
+++ ctp-trader/examples/auth.rs
@@ -31,13 +31,28 @@ fn new_login() -> CThostFtdcReqUserLoginField {
     f
 }
 
+fn new_password() -> CThostFtdcUserPasswordUpdateField {
+    let mut f: CThostFtdcUserPasswordUpdateField = Default::default();
+
+    let broker_id = "9999";
+    let user_id = &std::env::var("CTP_USER_ID").unwrap();
+    let old_password = &std::env::var("CTP_PASSWORD").unwrap();
+    let new_password = &std::env::var("CTP_NEW_PASSWORD").unwrap();
+
+    set_cstr_from_str_truncate(&mut f.BrokerID, broker_id);
+    set_cstr_from_str_truncate(&mut f.UserID, user_id);
+    set_cstr_from_str_truncate(&mut f.OldPassword, old_password);
+    set_cstr_from_str_truncate(&mut f.NewPassword, new_password);
+    f
+}
+
 fn main() {
     let mut current_request_id = 0;
     let flow_path = std::ffi::CString::new("").unwrap();
     let mut trader_api = TraderApi::new(flow_path);
 
     trader_api.register_spi(Box::new(Spi));
-    trader_api.register_front(std::ffi::CString::new("tcp://180.168.146.187:10130").unwrap());
+    trader_api.register_front(std::ffi::CString::new("tcp://180.168.146.187:10100").unwrap());
     trader_api.subscribe_private_topic(ResumeType::Quick);
     trader_api.subscribe_public_topic(ResumeType::Quick);
     trader_api.init();
@@ -54,6 +69,12 @@ fn main() {
     match trader_api.req_user_login(&new_login(), current_request_id) {
         Ok(()) => println!("req_user_login ok"),
         Err(err) => println!("req_user_login err: {:?}", err),
+    };
+
+    current_request_id += 1;
+    match trader_api.req_user_password_update(&new_password(), current_request_id) {
+        Ok(()) => println!("req_user_password_update ok"),
+        Err(err) => println!("req_user_password_update err: {:?}", err),
     };
 
     sleep(2);
